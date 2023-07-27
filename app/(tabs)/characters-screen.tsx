3c94930adadfd7def1695749487d8ae19a043d01
GustavoMont <lgmluisgm@gmail.com>
2023-07-27 19:51:28 -0300

    setup project

---
+++ app/(tabs)/characters-screen.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+import { Layout } from "@/components/common/layout/Layout";
+
+export default function CharactersScreen() {
+  return <Layout></Layout>;
+}
---
+++ components/common/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { View, useTheme } from "native-base";
+import React, { PropsWithChildren } from "react";
+
+export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  const { colors } = useTheme();
+  return (
+    <View flex={1} px={5} backgroundColor={colors.darkBlue[900]}>
+      {children}
+    </View>
+  );
+};
